909884e7876ab94f13b5ecfeed4702d4468600e2
Diego Jaimes <djaimes10@gmail.com>
2018-03-23 15:41:30 -0600

    Base de datos actualizada

--- prueba.sql
+++ prueba.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-03-2018 a las 02:06:02
--- Versión del servidor: 5.7.21-log
+-- Tiempo de generación: 23-03-2018 a las 22:40:56
+-- Versión del servidor: 5.7.20-log
 -- Versión de PHP: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -46,15 +46,23 @@ CREATE TABLE `alumnos` (
 --
 
 INSERT INTO `alumnos` (`matricula`, `nombreA`, `apellidoP`, `apellidoM`, `contrasena`, `grado`, `grupo`, `escuela`, `isActivo`, `rol`) VALUES
+('', '', '', '', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', 'UT', 1, 1),
+('ADAUT4q', 'Anthony', 'D', 'Angelo', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '4', 'q', 'UT', 1, 1),
 ('adrUTA', 'ariadna', 'diaz', 'roman', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '3', 'A', 'UT', 1, 1),
+('AJCBA4A', 'Austin', 'Jaimes', 'Cruz', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '4', 'A', 'BA', 1, 1),
 ('AMUUT4B', 'Adata', 'Memoria', 'USB', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '4', 'B', 'UT', 1, 1),
 ('cltUT3a', 'christian omar', 'lugo', 'tovar', '8cb2237d0679ca88db6464eac60da96345513964', '3', 'a', 'UT', 1, 1),
 ('djvuta', 'diego', 'jaimes', 'vazquez', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '3', 'a', 'PI', 1, 1),
 ('hcstecb', 'hola', 'como', 'stasss', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '3', 'b', 'BA', 1, 1),
 ('HJSNH3A', 'Heidi', 'Jimenez', 'Silva', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '3', 'A', 'NH', 1, 2),
+('HPLBA6A', 'Huawei', 'P10', 'Lite', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '6', 'A', 'BA', 1, 1),
+('HPLBA6B', 'Huawei', 'P10', 'Lite', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '6', 'B', 'BA', 1, 1),
 ('LMPUT4B', 'Laptop', 'Macbook', 'Pro', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '4', 'B', 'UT', 1, 1),
+('LPAPI6A', 'Luis', 'Perez', 'Avalos', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '6', 'A', 'PI', 1, 1),
 ('MPSUT5B', 'Manuel', 'Perez', 'Sosa', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '5', 'B', 'UT', 1, 1),
-('PPAPI4b', 'PEdro', 'Picapiedra', 'Alvarez', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '4', 'b', 'PI', 1, 1);
+('PPAPI4b', 'PEdro', 'Picapiedra', 'Alvarez', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '4', 'b', 'PI', 1, 1),
+('RJCBA3A', 'Rania Fernanda', 'Jaimes ', 'Cruz', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '3', 'A', 'BA', 1, 1),
+('TJVNH6B', 'Terry', 'Jaimes', 'Vazquez', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '6', 'B', 'NH', 1, 1);
 
 --
 -- Índices para tablas volcadas
