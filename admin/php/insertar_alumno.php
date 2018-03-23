2cdbc31ba110b53cce0155f6a65ac1bc1f7dea8d
Diego Jaimes <djaimes10@gmail.com>
2018-03-23 15:38:35 -0600

    Añadiendo la busqueda con filtros

--- admin/php/insertar_alumno.php
+++ admin/php/insertar_alumno.php
@@ -21,7 +21,8 @@ $contrasena = mysqli_real_escape_string($link, $_POST['contrasena']);
 // attempt insert query execution
 $sql = "INSERT INTO alumnos VALUES ('$matricula', '$nombreAlumno', '$apellidoPaterno', '$apellidoMaterno', sha1('$contrasena'), '$grado', '$grupo','$escuela', 1, 1)";
 if(mysqli_query($link, $sql)){
-    echo "<div class='alert alert-success'>Los datos del alumno se guardaron exitosamente. $sql </div>";
+    //echo "<div class='alert alert-success'>Los datos del alumno se guardaron exitosamente. $sql </div>";
+    echo "<script>alert('Los datos del alumno se guardaron exitosamente.');</script>";
 } else{
     echo "<div class='alert alert-danger'> Hubo un error al guardar los datos. Vuelve a intentarlo. $sql. </div>";
 }
